c2818839e6f54555b5a01a685246afbd7160e231
abdullahh545 <abdullahshaf49@gmail.com>
2025-07-16 23:34:45 +0300

    Final Commit: Full working 3x3 and 4x4 Tic Tac Toe with full UI, logic, sounds, and mode switch

--- script/app.js
+++ script/app.js
@@ -15,7 +15,6 @@ function init() {
   
 
 
-
   let board = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
   let gameOver = false
   let currentPlayer = 'X';
@@ -73,6 +72,9 @@ function init() {
 
   function handleClick(e) {
     const idx = e.target.id;
+    console.log(idx)
+    console.log(board[idx])
+    console.log(gameOver)
     if(board[idx] != ''){
       return
     }
@@ -96,7 +98,6 @@ function init() {
   function resetGame() {
     document.getElementById('reset-sound').play();
     board = Array(16).fill('');
-    gameOver = false;
     currentPlayer = 'X'
     rendorBoard()
     message.textContent = `${currentPlayer}'s turn`;
@@ -105,20 +106,13 @@ function init() {
   }
 
   square.forEach(sqr => sqr.addEventListener('click', handleClick))
-  resetBtn.addEventListener('click', () => {
-    if (isThreeByThree) {
-      resetGame3x3()
-    } else {
-      resetGame()
-    }
-  });
+  console.log(resetBtn)
+  resetBtn.addEventListener('click', resetGame);
 
   
 
   rendorBoard()
   message.textContent = `${currentPlayer}'s turn`;
-
-
 
 
 
@@ -135,3 +129,158 @@ function init() {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
+    [2, 4, 6]
+
+  ]
+
+
+
+
+
+
+  function create3x3Board() {
+    console.log("aa")
+    const container = document.querySelector('.grid3x3')
+    console.log(container)
+    if (container.children.length > 0) return
+    for (let i = 0; i < 9; i++) {
+      console.log('in for loop')
+      const square = document.createElement('div')
+      console.log(square)
+      square.classList.add('sqr3')
+      square.textContent = ''
+      square.id = `${i}`
+      square.addEventListener('Click', handleClick3x3)
+      container.appendChild(square)
+
+    }
+
+    square3 = document.querySelectorAll('.sqr3')
+
+    console.log(square3)
+
+    square3.forEach(sqr3 => sqr3.addEventListener('click', handleClick3x3))
+
+  }
+
+
+  btn()
+
+  function btn() {
+    btn3x3.addEventListener('click', () => {
+     isThreeByThree = true;
+     audioElem2.play()
+     resetGame3x3()
+      create3x3Board()
+      grid4x4.style.display = 'none'
+      grid3x3.style.display = 'grid'
+      create3x3Board()
+
+
+    })
+    
+
+    
+
+    console.log(btn3x3)
+
+    btn4x4.addEventListener('click', () => {
+      audioElem2.play()
+      isThreeByThree = false;
+      resetGame()
+      grid3x3.style.display = 'none'
+      grid4x4.style.display = 'grid'
+
+    })
+  }
+
+
+  function checkWinner3x3() {
+    for (let combo of winningsCombos3x3) {
+      const [a, b, c,] = combo;
+      if (board3x3[a] && board3x3[a] === board3x3[b] && board3x3[b] === board3x3[c]) {
+
+        gameOver3x3 = true;
+        message.textContent = `${board3x3[a]} wins!`;
+        return
+      }
+
+    }
+
+    if (!board3x3.includes('')) {
+      gameOver3x3 = true;
+      message.textContent = "It's a draw!";
+      return;
+    }
+
+  }
+
+  create3x3Board()
+
+  function handleClick3x3(e) {
+    const idx = Number(e.target.id)
+    console.log(board3x3)
+    console.log(board3x3[idx])
+    console.log(gameOver3x3)
+    if(board3x3[idx]!= '' )
+    {
+      return
+    }
+    if (gameOver3x3 || board3x3[''])
+
+      return;
+     audioElem.pause()
+      audioElem.currentTime = 0
+      audioElem.play()
+
+
+    board3x3[idx] = currentPlayer3x3;
+    rendorBoard3x3()
+    checkWinner3x3();
+
+    if (!gameOver3x3) {
+      currentPlayer3x3 = currentPlayer3x3 === 'X' ? 'O' : 'X';
+      message.textContent = `${currentPlayer3x3}'s turn`;
+    }
+  }
+  function rendorBoard3x3() {
+
+    board3x3.forEach((tile, index) => {
+      square3[index].textContent = tile
+    })
+  }
+
+  function resetGame3x3() {
+        document.getElementById('reset-sound').play();
+
+    board3x3 = Array(9).fill('');
+    currentPlayer3x3 = 'X'
+    gameOver3x3 = false
+    rendorBoard3x3()
+    message.textContent = `${currentPlayer3x3}'s turn`;
+
+
+  }
+
+  // square3.forEach(sqr3 => sqr3.addEventListener('click', handleClick3x3))
+  // console.log(square3)
+  console.log(resetBtn)
+  resetBtn.addEventListener('click', resetGame3x3);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', init)
+
+
+
